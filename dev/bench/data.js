a018a8d8b1e6beb8a0d21a876e5f82b8b40b6848
github-action-benchmark <github@users.noreply.github.com>
2025-01-10 17:23:47 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for b9418acc0bcee80e66f04c9aafa219f765d0eecd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736529783671,
+  "lastUpdate": 1736529827836,
   "repoUrl": "https://github.com/LayerZero-Labs/qmdb",
   "entries": {
     "Benchmark": [
@@ -58,6 +58,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "Block Population Throughput",
             "value": 49.327148103578324,
+            "unit": "ops/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dwong@layerzerolabs.org",
+            "name": "danielwlz",
+            "username": "danielwlz"
+          },
+          "committer": {
+            "email": "dwong@layerzerolabs.org",
+            "name": "danielwlz",
+            "username": "danielwlz"
+          },
+          "distinct": true,
+          "id": "b9418acc0bcee80e66f04c9aafa219f765d0eecd",
+          "message": "Fix broken link",
+          "timestamp": "2025-01-11T01:20:37+08:00",
+          "tree_id": "3b613c2a7882aa40fcfb98abc00be95336fd486f",
+          "url": "https://github.com/LayerZero-Labs/qmdb/commit/b9418acc0bcee80e66f04c9aafa219f765d0eecd"
+        },
+        "date": 1736529826545,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Transactions Throughput",
+            "value": 90908.43802121785,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Inserts Throughput",
+            "value": 471906.9476136994,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Reads Throughput",
+            "value": 231211.3217562372,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Reads Latency",
+            "value": 35,
+            "unit": "ns"
+          },
+          {
+            "name": "Deletes Throughput",
+            "value": 301141.08439928794,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Updates Throughput",
+            "value": 167049.68765667488,
+            "unit": "ops/s"
+          },
+          {
+            "name": "Block Population Throughput",
+            "value": 32.124818024952695,
             "unit": "ops/s"
           }
         ]
